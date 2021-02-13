49d0f35359a4b74fb80351686587a80c5cb56ac8
Dinuda Yaggahavita <ydinuda@gmail.com>
2021-02-13 12:24:33 +0530

    refresh screeen

--- kilo.c
+++ kilo.c
@@ -209,10 +209,11 @@ void editorRefreshScreen()
 {
     struct abuf ab = ABUF_INIT;
     abAppend(&ab, "\x1b[?25l", 6);
-    abAppend(&ab, "\x1b[2J", 4);
     abAppend(&ab, "\x1b[H", 3);
     editorDrawRows(&ab);
-    abAppend(&ab, "\x1b[H", 3);
+    char buf[32];
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+    abAppend(&ab, buf, strlen(buf));
     abAppend(&ab, "\x1b[?25h", 6);
     write(STDOUT_FILENO, ab.b, ab.len);
     abFree(&ab);
