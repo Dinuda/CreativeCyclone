b4ef5a9e1415882d272f01d0e0a016796fbb9ddc
Dinuda Yaggahavita <ydinuda@gmail.com>
2021-02-13 12:17:09 +0530

    init editor

--- kilo.c
+++ kilo.c
@@ -71,8 +71,11 @@ void die(const char *s)
 int getWindowSize(int *rows, int *cols)
 {
     struct winsize ws;
-    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
+    if (1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
     {
+        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)
+            return -1;
+        editorReadKey();
         return -1;
     }
     else
