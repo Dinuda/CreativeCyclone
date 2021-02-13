faba5aa98fcf13b0b46479dbf2738e6a848aa9c5
Dinuda Yaggahavita <ydinuda@gmail.com>
2021-02-13 11:57:46 +0530

    add cli config

--- kilo.c
+++ kilo.c
@@ -1,4 +1,47 @@
+// Imports
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+struct termios orig_termios;
+
+// Enable raw mode and turn off echo
+void enableRawMode()
+{
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
+    struct termios raw = orig_termios;
+    // Fic CTRL+M
+    raw.c_iflag &= ~(ICRNL | IXON);
+    // Disable CTRL+S and CTRL+Q
+    raw.c_iflag &= ~(IXON);
+    // local flag and disable CTRL-V
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+// Disable raw mode and turn off echo
+void disableRawMode()
+{
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
 int main()
 {
-    return 1;
+    enableRawMode();
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
+    {
+        if (iscntrl(c))
+        {
+            printf("%d\n", c);
+        }
+        else
+        {
+            printf("%d ('%c')\n", c, c);
+        }
+    }
+    return 0;
 }
