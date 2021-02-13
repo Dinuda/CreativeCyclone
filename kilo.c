02f11bd0205bde843ebbf2010172518d6d80cff2
Dinuda Yaggahavita <ydinuda@gmail.com>
2021-02-13 12:10:49 +0530

    clear screen on exit

--- kilo.c
+++ kilo.c
@@ -71,6 +71,8 @@ void editorProcessKeypress()
     switch (c)
     {
     case CTRL_KEY('q'):
+        write(STDOUT_FILENO, "\x1b[2J", 4);
+        write(STDOUT_FILENO, "\x1b[H", 3);
         exit(0);
         break;
     }
