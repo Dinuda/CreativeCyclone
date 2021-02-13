ce44dd52ca9720ef95644e9b6e0ac357e7935450
Dinuda Yaggahavita <ydinuda@gmail.com>
2021-02-13 12:23:11 +0530

    welcome message

--- kilo.c
+++ kilo.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
@@ -109,78 +110,110 @@ int getWindowSize(int *rows, int *cols)
             return 0;
         }
     }
+}
 
-    // Refactor keyboard read
-    char editorReadKey()
+// Refactor keyboard read
+char editorReadKey()
+{
+    int nread;
+    char c;
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1)
     {
-        int nread;
-        char c;
-        while ((nread = read(STDIN_FILENO, &c, 1)) != 1)
-        {
-            if (nread == -1 && errno != EAGAIN)
-                die("read");
-        }
-        return c;
+        if (nread == -1 && errno != EAGAIN)
+            die("read");
     }
+    return c;
+}
 
-    // Refactor keyboard input
-    void editorProcessKeypress()
+// Refactor keyboard input
+void editorProcessKeypress()
+{
+    char c = editorReadKey();
+    switch (c)
     {
-        char c = editorReadKey();
-        switch (c)
+    case CTRL_KEY('q'):
+        write(STDOUT_FILENO, "\x1b[2J", 4);
+        write(STDOUT_FILENO, "\x1b[H", 3);
+        exit(0);
+        break;
+    }
+}
+
+// Tidles
+void editorDrawRows(struct abuf *ab)
+{
+    int y;
+    for (y = 0; y < E.screenrows; y++)
+    {
+        if (y == E.screenrows / 3)
         {
-        case CTRL_KEY('q'):
-            write(STDOUT_FILENO, "\x1b[2J", 4);
-            write(STDOUT_FILENO, "\x1b[H", 3);
-            exit(0);
-            break;
+            char welcome[80];
+            int welcomelen = snprintf(welcome, sizeof(welcome),
+                                      "Kilo editor -- version %s", KILO_VERSION);
+            if (welcomelen > E.screencols)
+                welcomelen = E.screencols;
+            abAppend(ab, welcome, welcomelen);
+        }
+        else
+        {
+            abAppend(ab, "~", 1);
+        }
+        abAppend(ab, "\x1b[K", 3);
+        if (y < E.screenrows - 1)
+        {
+            abAppend(ab, "\r\n", 2);
         }
     }
+}
 
-    // Tidles
-
-    void editorDrawRows()
-    {
-        int y;
-        for (y = 0; y < E.screenrows; y++)
-        {
-            write(STDOUT_FILENO, "~", 1);
-            if (y < E.screenrows - 1)
-            {
-                write(STDOUT_FILENO, "\r\n", 2);
-            }
-        }
-    }
-
-    /*** append buffer ***/
-    struct abuf
-    {
-        char *b;
-        int len;
-    };
+/*** append buffer ***/
+struct abuf
+{
+    char *b;
+    int len;
+};
 #define ABUF_INIT \
     {             \
         NULL, 0   \
     }
 
-    // Refresh the screen
-    void editorRefreshScreen()
-    {
-        write(STDOUT_FILENO, "\x1b[2J", 4);
-        write(STDOUT_FILENO, "\x1b[H", 3);
+void abAppend(struct abuf *ab, const char *s, int len)
+{
+    char *new = realloc(ab->b, ab->len + len);
+    if (new == NULL)
+        return;
+    memcpy(&new[ab->len], s, len);
+    ab->b = new;
+    ab->len += len;
+}
+void abFree(struct abuf *ab)
+{
+    free(ab->b);
+}
 
-        editorDrawRows();
-        write(STDOUT_FILENO, "\x1b[H", 3);
-    }
+// Refresh the screen
 
-    int main()
+void editorRefreshScreen()
+{
+    struct abuf ab = ABUF_INIT;
+    abAppend(&ab, "\x1b[?25l", 6);
+    abAppend(&ab, "\x1b[2J", 4);
+    abAppend(&ab, "\x1b[H", 3);
+    editorDrawRows(&ab);
+    abAppend(&ab, "\x1b[H", 3);
+    abAppend(&ab, "\x1b[?25h", 6);
+    write(STDOUT_FILENO, ab.b, ab.len);
+    abFree(&ab);
+}
+
+int main()
+{
+    enableRawMode();
+    initEditor();
+    while (1)
     {
-        enableRawMode();
-        initEditor();
-        while (1)
-        {
-            editorRefreshScreen();
-            editorProcessKeypress();
-        }
-        return 0;
+        editorRefreshScreen();
+        editorProcessKeypress();
     }
+    return 0;
+}
