cd7147f3be0bed2d2df46f2dcbde9b14439730d9
Dinuda Yaggahavita <ydinuda@gmail.com>
2021-02-13 11:59:01 +0530

    turn off all processing

--- kilo.c
+++ kilo.c
@@ -38,11 +38,11 @@ int main()
     {
         if (iscntrl(c))
         {
-            printf("%d\n", c);
+            printf("%d\r\n", c);
         }
         else
         {
-            printf("%d ('%c')\n", c, c);
+            printf("%d ('%c')\r\n", c, c);
         }
     }
     return 0;
