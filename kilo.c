55012c54a15b91dd2e818deee08f01eca94ef5cd
Dinuda Yaggahavita <ydinuda@gmail.com>
2021-02-13 12:07:44 +0530

    edit main

--- kilo.c
+++ kilo.c
@@ -48,24 +48,36 @@ void die(const char *s)
     exit(1);
 }
 
+// Refactor keyboard Input
+char editorReadKey()
+{
+    int nread;
+    char c;
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1)
+    {
+        if (nread == -1 && errno != EAGAIN)
+            die("read");
+    }
+    return c;
+}
+/*** input ***/
+void editorProcessKeypress()
+{
+    char c = editorReadKey();
+    switch (c)
+    {
+    case CTRL_KEY('q'):
+        exit(0);
+        break;
+    }
+}
+
 int main()
 {
     enableRawMode();
     while (1)
     {
-        char c = '\0';
-        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
-            die("read");
-        if (iscntrl(c))
-        {
-            printf("%d\r\n", c);
-        }
-        else
-        {
-            printf("%d ('%c')\r\n", c, c);
-        }
-        if (c == CTRL_KEY('q'))
-            break;
+        editorProcessKeypress();
     }
     return 0;
 }
