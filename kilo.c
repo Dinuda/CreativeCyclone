a0642deba7ee22cb58c4fa8d10e33f3d891f6039
Dinuda Yaggahavita <ydinuda@gmail.com>
2021-02-13 12:02:18 +0530

    A timeout for read

--- kilo.c
+++ kilo.c
@@ -40,9 +40,10 @@ void disableRawMode()
 int main()
 {
     enableRawMode();
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
+    while (1)
     {
+        char c = '\0';
+        read(STDIN_FILENO, &c, 1);
         if (iscntrl(c))
         {
             printf("%d\r\n", c);
@@ -51,6 +52,8 @@ int main()
         {
             printf("%d ('%c')\r\n", c, c);
         }
+        if (c == 'q')
+            break;
     }
     return 0;
 }
