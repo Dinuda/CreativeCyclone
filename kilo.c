6444783e53fbac4920e8d73572950ab97199db50
Dinuda Yaggahavita <ydinuda@gmail.com>
2021-02-13 12:10:25 +0530

    clear screen on exit

--- kilo.c
+++ kilo.c
@@ -44,6 +44,9 @@ void disableRawMode()
 // Error Handling
 void die(const char *s)
 {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
     perror(s);
     exit(1);
 }
